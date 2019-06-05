49bbe4e1911afe4f733761ddb6bbb1b1b36e2b98
Hamurrabi <48670186+Hamurrabi@users.noreply.github.com>
2019-06-05 07:29:52 -0600

    grid layout

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double num1 = 0;
+        double num2 = 0;
+
+        string op = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //number buttons
+
+        private void btnNum_Click(object sender, RoutedEventArgs e)
+
+        {
+            Button btn = (Button)sender;
+            if (screen.Text == "0")
+            {
+                screen.Text = btn.Content.ToString();
+            }
+            else
+            {
+                screen.Text += btn.Content.ToString();
+            }
+        }
+
+        //operator buttons
+
+        private void btnOp_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            op = btn.Content.ToString();
+            num1 = double.Parse(screen.Text);
+            screen.Text = "0";
+
+        }
+
+        private void BEqu_Click(object sender, RoutedEventArgs e)
+        {
+            num2 = double.Parse(screen.Text);
+            switch(op)
+            {
+                case "+":
+                    screen.Text = (num1 + num2).ToString();
+                    break;
+                case "-":
+                    screen.Text = (num1 - num2).ToString();
+                    break;
+                case "*":
+                    screen.Text = (num1 * num2).ToString();
+                    break;
+                case "/":
+                    screen.Text = (num1 / num2).ToString();
+                    break;
+
+            }
+
+        }
+
+        private void BCE_Click(object sender, RoutedEventArgs e)
+        {
+            if (op == "")
+            {
+                num1 = 0;
+                screen.Text = "0";
+            }
+            else
+            {
+                num2 = 0;
+                screen.Text = "0";
+            }
+        }
+
+        private void BDec_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text = screen.Text + ".";
+        }
+        
+        private void BC_Click(object sender, RoutedEventArgs e)
+        {
+            num1 = 0; num2 = 0; op = "";
+            screen.Text = "0";
+        }
+
+        private void BPosNeg_Click(object sender, RoutedEventArgs e)
+        {
+            if (screen.Text.StartsWith("-"))
+            {
+                screen.Text = screen.Text.Substring(1);
+            }
+            else if(!string.IsNullOrEmpty(screen.Text) && double.Parse(screen.Text) != 0)
+            {
+                screen.Text = "-" + screen.Text;
+            }
+        }
+    }
+}
