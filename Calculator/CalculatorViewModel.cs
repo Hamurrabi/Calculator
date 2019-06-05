d4499fe6357a9eb90eddce7d2c8b79a4cb62ce69
Mark Feldman <markfeldman@chartlogic.com>
2019-06-05 11:07:47 -0600

    Partially through moving functionality into MVVM

--- Calculator/CalculatorViewModel.cs
+++ Calculator/CalculatorViewModel.cs
@@ -1,5 +1,79 @@
-﻿namespace Calculator {
-  public class CalculatorViewModel {
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Calculator.Annotations;
 
+namespace Calculator {
+  public class CalculatorViewModel : INotifyPropertyChanged {
+    private double _num1;
+    private double _num2;
+    private string _operation; //note, varible names matter - had to search to figure out what op meant
+    private string _answer;
+
+    public double Num1 {
+      get { return _num1; }
+      set {
+        _num1 = value;
+        OnPropertyChanged(nameof(Num1));
+      }
+    }
+
+    public double Num2 {
+      get { return _num2; }
+      set {
+        _num2 = value;
+        OnPropertyChanged(nameof(Num2));
+      }
+    }
+
+    public string Operation {
+      get { return _operation; }
+      set {
+        _operation = value;
+        OnPropertyChanged(nameof(Operation));
+      }
+    }
+
+    public string Answer {
+      get { return _answer; }
+      set {
+        _answer = value;
+        OnPropertyChanged(nameof(Answer));
+      }
+    }
+
+    public ICommand ButtonCommand;
+
+    public CalculatorViewModel() {
+      RegisterCommands();
+    }
+
+    private void RegisterCommands() {
+      ButtonCommand = new RelayCommand(ExecuteButtonCommand);
+    }
+
+    private void ExecuteButtonCommand(object parameter) {
+      var key = (string) parameter;
+      switch (key) {
+        case "CE":
+          if (Operation == string.Empty) {
+            Num1 = 0;
+          }
+          else {
+            Num2 = 0;
+          }
+          Answer = "0";
+          break;
+
+      }
+    }
+
+    //Stuff needed for INotifyPropertyChanged to work
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    [NotifyPropertyChangedInvocator]
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
   }
 }
---
+++ Calculator/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Calculator {
+  //This class is a generic template used to help implement MVVM
+  //It is a very good working example, though it may not always accomodate all needs
+  public class RelayCommand : ICommand {
+    public event EventHandler CanExecuteChanged {
+      add { CommandManager.RequerySuggested += value; }
+      remove { CommandManager.RequerySuggested -= value; }
+    }
+
+    private readonly Action<object> methodToExecute;
+    private readonly Func<bool> canExecuteEvaluator;
+
+    public RelayCommand(Action<object> methodToExecute, Func<bool> canExecuteEvaluator) {
+      this.methodToExecute = methodToExecute;
+      this.canExecuteEvaluator = canExecuteEvaluator;
+    }
+
+    public RelayCommand(Action<object> methodToExecute) : this(methodToExecute, null) { }
+
+    public bool CanExecute(object parameter) {
+      if (canExecuteEvaluator == null) {
+        return true;
+      }
+      else {
+        bool result = canExecuteEvaluator.Invoke();
+        return result;
+      }
+    }
+
+    public void Execute(object parameter) {
+      methodToExecute(parameter);
+    }
+  }
+}
